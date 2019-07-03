40ac5ab462b200cce722b06c7df9e8dc8411a9de
ZHShang <zhihengshang@hotmail.com>
2019-07-03 00:50:55 +0000

    Final edit of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ User.create!({
  is_admin: true,
  current_roundup_balance: 20020.35,
  total_balance: 20020.35,
- votes: [35,62,11,36,101]
+ votes: [35,22,40,36,101]
 })
 
 #Charities
@@ -90,71 +90,71 @@ puts char1
 Goal.destroy_all
 
 char1.goals.create!({
-  objective: 'Build 3 MRI Scanners',
+  objective: 'SickKids - Build a MRI Scanner',
   desc: 'Building 3 additional MRI Scanners helps reduce the process of wait',
-  cost: 10000,
+  cost: 8000,
   completed: false
 })
 
 char1.goals.create!({
-  objective: 'Build a playground',
+  objective: 'SickKids - Build a playground',
   desc: 'Building a playground for kids to play in',
   cost: 2000,
   completed: false
 })
 
 char2.goals.create!({
-  objective: 'Cancer Research',
+  objective: 'PMH - Hire More Researchers',
   desc: 'Hire more researchers to help speed of tests',
-  cost: 20000,
+  cost: 10000,
   completed: false
 })
 
 char2.goals.create!({
-  objective: 'New facility equipments',
+  objective: 'PMH - Buy new facility equipments',
   desc: 'Acquire more equipments for more accessible tools',
   cost: 3000,
   completed: false
 })
 
 char3.goals.create!({
-  objective: 'Beta-Testing Facilities',
+  objective: 'Parkinsons - Build a Recreation Room',
   desc: 'New tools needed to better testing before launch of new technologies',
-  cost: 10000,
+  cost: 6000,
   completed: false
 })
 
 char3.goals.create!({
-  objective: 'More researchers',
-  desc: 'Hire more researchers',
-  cost: 3000,
+  objective: 'Parkinsons - Hire More Nurses',
+  desc: 'Hire more nurses',
+  cost: 5000,
   completed: false
 })
 
 char4.goals.create!({
-  objective: 'More land ownership',
+  objective: 'HFH - Buy New Property',
   desc: 'More land meaning more houses can be built',
   cost: 60000,
   completed: false
 })
 
 char4.goals.create!({
-  objective: 'More materials',
+  objective: 'HFH - Buy More Building Materials',
   desc: 'More material allocated to build more houses',
-  cost: 30000,
+  cost: 10000,
   completed: false
 })
 
 char5.goals.create!({
-  objective: 'More workers',
+  objective: 'DBF - Hire More Chefs',
   desc: 'More workers can be allocated to get a wider scope of food gathering',
-  cost: 20000,
+  cost: 8000,
   completed: false
 })
 
 char5.goals.create!({
-  objective: 'More builders',
-  desc: 'More builders to produce houses faster!',
+  objective: 'DBF - Sign Deals with Additional Grocery Stores',
+  desc: 'More food for the people',
   cost: 10000,
   completed: false
 })
